9b5fbe69f8770ef22a1d1155c66d6f486c3b65f0
Ryhor-Karnatsevich <spamjamesmorgan@gmail.com>
2024-11-24 22:20:17 +0000

    1

---
+++ numpyy.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib as plt
+#np.random.seed(123)
+coin = np.random.rand(0,2)    #like a coin that depends on seed
+dice = np.random.randint(1,7) #randomly flips a dice
+
+
+
